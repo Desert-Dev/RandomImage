3fbc9cc0d6778e490601c791dd869f29e06ea8c7
Santiago Aguirre <santiagoagdc42@gmail.com>
2020-06-22 13:45:04 -0700

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Santiago Gomez del campo on 22/06/20.
+//  Copyright © 2020 Santiago Gomez del campo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
