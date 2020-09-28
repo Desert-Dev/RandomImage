29ac1b24428bfeb5fdf8d7e57d5e10050acee701
Santiago Aguirre <santiagoagdc42@gmail.com>
2020-09-27 21:04:00 -0700

    sync github with loca xcode

---
+++ RandomPhoto/Model/ImageNetworking.swift
@@ -0,0 +1,9 @@
+//
+//  ImageNetworking.swift
+//  RandomPhoto
+//
+//  Created by Santiago Gomez del campo on 25/06/20.
+//  Copyright © 2020 Santiago Gomez del campo. All rights reserved.
+//
+
+import Foundation
